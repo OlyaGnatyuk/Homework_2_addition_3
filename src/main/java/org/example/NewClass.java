61847c17df6243059b6525914be85c82e14acdf5
OlyaGnatyuk <43353123+OlyaGnatyuk@users.noreply.github.com>
2022-11-26 22:30:25 +0300

    Initial commit

---
+++ src/main/java/org/example/NewClass.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class NewClass {
+    private int num = 10;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int number) {
+        this.num = number;
+    }
+}
